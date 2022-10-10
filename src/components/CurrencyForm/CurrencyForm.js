e8e30ed57faa0402efa2be4e5706282825a79287
MartynaM97 <110252592+MartynaM97@users.noreply.github.com>
2022-10-10 10:59:14 +0200

    Add files via upload

---
+++ src/components/CurrencyForm/CurrencyForm.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import TextInput from './../TextInput/TextInput';
+import Select from './../Select/Select';
+import Button from './../Button/Button';
+import styles from './CurrencyForm.module.scss';
+
+const CurrencyForm = ({ action }) => {
+  const [amount, setAmount] = useState('');
+  const [from, setFrom] = useState('PLN');
+  const [to, setTo] = useState('PLN');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    action({ 
+      amount: parseInt(amount),
+      from,
+      to,
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label>
+        <span>Amount:</span>
+        <TextInput
+          data-testid="amount"
+          type="number" value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
+      </label>
+      <label>
+        <span>From</span>
+        <Select
+          data-testid="from-select"
+          onChange={(event) => setFrom(event.target.value)}>
+          <option value="PLN">PLN</option>
+          <option value="USD">USD</option>
+        </Select>
+      </label>
+      <label>
+        <span>To</span>
+        <Select
+          data-testid="to-select"
+          onChange={(event) => setTo(event.target.value)}>
+          <option value="PLN">PLN</option>
+          <option value="USD">USD</option>
+        </Select>
+      </label>
+      <Button>Convert</Button>
+    </form>
+  );
+};
+
+export default CurrencyForm;
